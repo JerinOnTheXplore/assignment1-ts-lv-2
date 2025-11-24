6ed7811b6e9bda10069731fbac0213ef8f1553c6
JerinXplore <jerinjerin101325@gmail.com>
2025-11-24 10:53:57 +0600

    added solution for Problem 2: getLength function

--- solution.ts
+++ solution.ts
@@ -12,3 +12,12 @@ function formatValue(value: string | number | boolean): string | number | boolea
 }
 
 
+
+function getLength(value: string | any[]) : number {
+    if (typeof value === "string"){
+        return value.length;
+    } else if (Array.isArray(value)) {
+        return value.length;
+    }
+    return 0;
+}
