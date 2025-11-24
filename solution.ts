d3615ae092a7d57e5fba64de462f2faa9f58825a
JerinXplore <jerinjerin101325@gmail.com>
2025-11-24 19:48:08 +0600

    added solution for Problem 7: getUniqueValues function

--- solution.ts
+++ solution.ts
@@ -80,6 +80,42 @@ function printBookDetails(book:Book): void {
 
 
 
+function getUniqueValues(
+    arr1: (number | string)[],
+    arr2: (number | string)[]
+): (number | string)[]{
+    const result: (number | string)[] = [] ;
+
+    for (let i = 0; i < arr1.length; i++) {
+        let exists =false;
+        for(let j=0; j < result.length; j++){
+            if(result[j] === arr1[i]) {
+                exists = true ;
+                break;
+            }
+        }
+        if(!exists) {
+        result[result.length]= arr1[i];
+        }
+    }
+    
+    for (let i=0; i < arr2.length; i++){
+        let exists = false;
+        for (let j=0; j < result.length; j++) {
+            if (result[j] === arr2[i]) {
+                exists = true;
+                break;
+            }
+        }
+        if (!exists) {
+            result[result.length] = arr2[i];
+        }
+    }
+    return result;
+}
+
+
+
 
 
 
